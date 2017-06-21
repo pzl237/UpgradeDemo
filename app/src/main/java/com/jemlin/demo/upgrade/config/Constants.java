3baf59773c78b3d111dbd86295e63bddb113ea60
panzhilong <420985926@qq.com>
2017-06-21 22:01:25 +0800

    update

--- app/src/main/java/com/jemlin/demo/upgrade/config/Constants.java
+++ app/src/main/java/com/jemlin/demo/upgrade/config/Constants.java
@@ -6,7 +6,7 @@ package com.jemlin.demo.upgrade.config;
 public class Constants {
 
     public static class App {
-        public static final String API_DOMAIN_DEBUG = "配置你自己的服务器基地址";
+        public static final String API_DOMAIN_DEBUG = "http://rapapi.org/mockjs/21104";
         public static final String API_DOMAIN_RELEASE = "配置你自己的服务器基地址";
 
         public static final int UPGRADE_DELAY_TIME = 1000; //在首页延迟1s启动检测升级
